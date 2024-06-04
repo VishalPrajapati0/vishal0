0c418894a850051bd08dd7ba15894b6a12d01169
Prajapati  Vishal <161322559+VishalPrajapati0@users.noreply.github.com>
2024-06-04 18:34:55 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,11 +15,29 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const host = '192.168.1.68';
-  const port = 3000;
-  await app.listen(port, host, () => {
-    console.log(`Nest application is running on http://${host}:${port}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Nest application is running on http://localhost:${port}`);
   });
-  
 }
+
 bootstrap();
+
+// Load environment variables
+dotenv.config();
+
+// PostgreSQL pool configuration
+const pool = new Pool({
+  connectionString:  process.env.vishal0_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  // Adjust SSL configuration based on your PostgreSQL server setup
+});
+
+// Example usage of the pool
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error executing query', err.stack);
+  } else {
+    console.log('PostgreSQL connected:', res.rows[0]);
+  }
+});
